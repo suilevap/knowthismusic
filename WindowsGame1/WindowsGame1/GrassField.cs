466955798c71b54dda5120a2b3eaa5c84309d907
suilevap <suilevap@localhost>
2012-04-06 17:03:07 +0300

    Added Grass class

---
+++ WindowsGame1/WindowsGame1/GrassField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace WindowsGame1
+{
+    class GrassField
+    {
+
+        private List<Grass> data;
+
+        public GrassField(Rectangle rect, int n)
+        {
+            data = new List<Grass>();
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                Vector2 pos = new Vector2(rnd.Next(rect.Left, rect.Right), rnd.Next(rect.Top, rect.Bottom));
+                float l = rnd.Next(rect.Height/2, rect.Height/2);
+                float k1 = (float) (0.2 + 0.2*rnd.NextDouble());
+                float k2 = (float) (k1 + 0.2 - 0.4*rnd.NextDouble());
+                Grass grass = new Grass(pos, new Vector2(l, l), new Vector2(k1, k2), 0.98f);
+            }
+        }
+    }
+}
