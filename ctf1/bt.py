06ca8e88699e65596e87306ebff06b5755adc796
suilevap <suilevap@localhost>
2012-11-24 01:46:55 +0300

    Refactoring

--- ctf1/bt.py
+++ ctf1/bt.py
@@ -107,9 +107,8 @@ class BTAction(BTNode):
     """
     Atomic action
     """
-    def __init__(self, action ):#, guardCondition= None):
+    def __init__(self, action ):
         self.action = action  
-        #self.guardCondition = guardCondition
         self.childs=[]      
 
     def execute(self, context):
@@ -119,15 +118,8 @@ class BTAction(BTNode):
         #commander, bot = context.executionContext
         #commander.log.info("Action run")    
         
-        #if (self.guardCondition != None):
-        #    condCheck = self.guardCondition(*context.executionContext)
-        #    if (not condCheck):
-        #        return BTNode.STATUS_OK
-
+  
         check = self.action(*context.executionContext);
-        #context.currentRunningNodeId = self.id
-        #return BTNode.STATUS_RUNNING
-
         #for easier using, actions cannot be failed
         if (check or check==None):
             #assume only one action can be running at one time
--- ctf1/pandsbot.py
+++ ctf1/pandsbot.py
@@ -39,7 +39,6 @@ class PandSBot(Commander):
         self.verbose = True    # display the command descriptions next to the bot labels
         for bot in self.game.team.members:            
             bot.role=PandSBot.ROLE_NONE
-            bot.commandQueue = deque()
             bot.brain = None
         self.defenderPart = 0.25
         self.countBot = len(self.game.team.members)
@@ -64,56 +63,7 @@ class PandSBot(Commander):
         for bot in self.game.bots_alive:
             if (bot.brain != None):
                 bot.brain.tick()
-            self.updateBotCommandQueue(bot)
-        
-        #defenders = self.botsInRole(PandSBot.ROLE_DEFENDER)
-        #for bot in defenders:
-        #    if len(bot.visibleEnemies)>0 and bot.state!=BotInfo.STATE_SHOOTING:
-        #        self.clearCommands(bot)
-        #        #self.enqueCommand(bot,lambda s,b:Command_DefendDirection(s, b, b.position)) 
-        #        self.issue(commands.Defend, bot, bot.visibleEnemies[0].position-bot.position, description='AAA')
-        #    elif self.botFree(bot):
-        #        #self.issue(commands.Move, bot, self.freePos(pos), description = 'Go to my flag (DEFENDER)')
-        #        #self.issue(commands.Defend, bot, self.game.enemyTeam.flag.position-bot.position , description = 'Defend my flag (DEFENDER)')
-        #        self.clearCommands(bot)
-        #        self.enqueCommand(bot,Command_MoveToMyFlag) 
-        #        self.enqueCommand(bot,Command_DefendMyFlag) 
-
-        #                
-        #attackers = self.botsInRole(PandSBot.ROLE_ATTACKER)
-        #for bot in attackers:
-        #    if not bot.flag and len(bot.visibleEnemies)>0 and bot.state!=BotInfo.STATE_SHOOTING:
-        #        self.clearCommands(bot)
-        #        #self.enqueCommand(bot,lambda s,b:Command_AttackBot(s,b, b)) 
-        #        self.issue(commands.Attack, bot, bot.visibleEnemies[0].position, description='BB')
-        #      
-        #    elif self.botFree(bot):
-        #        if bot.flag:
-        #            # Tell the flag carrier to run home!
-        #            target = self.game.team.flagScoreLocation
-        #            self.clearCommands(bot)
-        #            self.enqueCommand(bot, Command_RunHome)
-        #        else:
-        #            pos = self.game.enemyTeam.flag.position 
-        #            #self.issue(commands.Move, bot, self.freePos((self.game.team.flag.position+pos)/2), description = 'Run to enemy flag (ATTACKER)')
-        #            self.clearCommands(bot)
-        #            self.enqueCommand(bot, Command_RunToMidPoint)
-        #            self.enqueCommand(bot, Command_AttackEnemyFlag)  
-
-        ## for all bots which aren't currently doing anything
-        #for bot in self.game.bots_available:
-
-        #
-
-        #    if bot.flag:
-        #        # if a bot has the flag run to the scoring location
-        #        flagScoreLocation = self.game.team.flagScoreLocation
-        #        self.issue(commands.Charge, bot, flagScoreLocation, description = 'Run to my flag')
-        #    else:
-        #        # otherwise run to where the flag is
-        #        enemyFlag = self.game.enemyTeam.flag.position
-        #        self.issue(commands.Charge, bot, enemyFlag, description = 'Run to enemy flag')
-
+    
     def shutdown(self):
         """Use this function to teardown your bot after the game is over, or perform an
         analysis of the data accumulated during the game."""
@@ -121,30 +71,11 @@ class PandSBot(Commander):
         pass
 
 
-
     def getBotNearestToPoint(self,position):
         return min(self.game.bots_alive, key=lambda bot: (bot.position-position).length())
 
     def botsInRole(self, role):
         return [bot for bot in self.game.bots_alive if bot.role==role]
-
-    def clearCommands(self, bot):
-        bot.commandQueue.clear();
-
-    def botFree(self, bot):
-        return bot.state == BotInfo.STATE_IDLE and len(bot.commandQueue)==0;
-
-    #def enqueCommand(self, CommandClass, bot, *args, **dct):
-    #    bot.commandQueue.append(CommandClass(bot.name, *args, **dct))
-    def enqueCommand(self, bot, action):
-        bot.commandQueue.append(action)
-
-    def updateBotCommandQueue(self, bot):
-        if bot.state == BotInfo.STATE_IDLE and len(bot.commandQueue)>0:
-            nextOrder = bot.commandQueue.popleft()
-            nextOrder(self, bot)
-            #self.issue(nextOrder[0], bot, nextOrder[1], nextOrder[2])
-            #self.commandQueue.append(nextOrder())
 
 
     def freePos(self, pos):
